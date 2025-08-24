bc216a6f739daee3b9e14dcfc573afa0d392e153
pakito312 <40355919+pakito312@users.noreply.github.com>
2025-08-24 16:26:24 +0100

    Add src/config/galleryImages.ts

---
+++ src/config/galleryImages.ts
@@ -0,0 +1,65 @@
+
+// Configuration des images de la galerie pour la page Restaurant
+export const restaurantGalleryImages = [
+  {
+    src: '/uploads/03f7af76-87c5-4cf8-a2ae-1fb48a0ff5e1.png',
+    alt: 'Vue du restaurant sur l\'eau au crépuscule'
+  },
+  {
+    src: '/uploads/57f03e81-eecb-464b-a0c3-5eccf1b1c77f.png',
+    alt: 'Espace détente extérieur avec coucher de soleil'
+  },
+  {
+    src: '/uploads/1adddd21-4d51-4dde-8b34-60330b106659.png',
+    alt: 'Passerelle du restaurant au-dessus de l\'eau'
+  },
+  {
+    src: '/uploads/50218001-9257-4d1e-adf9-4c4af1778819.png',
+    alt: 'Cabane traditionnelle sur pilotis avec toit de paille'
+  },
+  {
+    src: '/uploads/41c9a456-0086-4074-82dc-b5aeea39c96a.png',
+    alt: 'Structure traditionnelle sur pilotis'
+  },
+  {
+    src: '/uploads/106b2d75-d369-47f8-a8c3-fcdd945af53f.png',
+    alt: 'Vue sur les installations depuis le rivage'
+  },
+  {
+    src: '/uploads/0da3af2e-bc3a-4d80-99c2-08c477c1efcd.png',
+    alt: 'Présentation élégante d\'un plat signature'
+  },
+  {
+    src: '/uploads/ed0b7f2e-cb9d-41a1-b63c-fad702a1b92f.png',
+    alt: 'Ambiance nocturne du restaurant'
+  },
+  {
+    src: '/uploads/a03a930d-3c13-4f04-a99e-9b07a67efd98.png',
+    alt: 'Espace privatif avec vue panoramique'
+  },
+  {
+    src: '/uploads/b32e9ef5-5dcf-4b63-b7d5-b95d3d2efadc.png',
+    alt: 'Détail architectural d\'une cabane traditionnelle'
+  },
+  {
+    src: '/uploads/8985b330-4c7f-4d9d-a648-45d3fbe0c7d3.png',
+    alt: 'Éléments décoratifs du restaurant'
+  },
+  {
+    src: '/uploads/106b2d75-d369-47f8-a8c3-fcdd945af53f.png',
+    alt: 'Infrastructure du restaurant au bord de l\'eau'
+  },
+];
+
+// Configuration des images pour la page Accueil
+export const homeGalleryImages = [
+  '/uploads/03f7af76-87c5-4cf8-a2ae-1fb48a0ff5e1.png',
+  '/uploads/57f03e81-eecb-464b-a0c3-5eccf1b1c77f.png',
+  '/uploads/1adddd21-4d51-4dde-8b34-60330b106659.png',
+  '/uploads/50218001-9257-4d1e-adf9-4c4af1778819.png',
+  '/uploads/41c9a456-0086-4074-82dc-b5aeea39c96a.png',
+  '/uploads/106b2d75-d369-47f8-a8c3-fcdd945af53f.png',
+  '/uploads/0da3af2e-bc3a-4d80-99c2-08c477c1efcd.png',
+  '/uploads/ed0b7f2e-cb9d-41a1-b63c-fad702a1b92f.png',
+  '/uploads/a03a930d-3c13-4f04-a99e-9b07a67efd98.png',
+];
